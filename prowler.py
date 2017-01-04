2a414c4667884184f765b1962bf60925c888c1ac
Eddog53 <eddog53@hotmail.com>
2017-01-03 16:35:04 -0800

    changed most things to best practice- 4 spaces instead of 2 double spaced in between functions lowercase function and variable names using all single quotes

--- prowler.py
+++ prowler.py
@@ -1,29 +1,29 @@
 import github
 import json
 
-def processRepoContents(repoContents):
-  # for each entry in the repo
-  for tree in repoContents['tree']:
-    contentType = tree['type']
-    print 'contentType --- ', contentType
-    # if type is "blob" get the content
-    if contentType == 'blob':
-      github.getFileContent(tree)
-      print '***blob***'
-    elif contentType == 'tree':
-      print '***tree***'
-      # if type is "tree" get the subtree
+
+def process_repo_contents(repo_contents):
+    # for each entry in the repo
+    for tree in repo_contents['tree']:
+        content_type = tree['type']
+        print('content_type --- ', content_type)
+        # if type is "blob" get the content
+        if content_type == 'blob':
+            github.get_file_content(tree)
+            print('***blob***')
+        elif content_type == 'tree':
+            print('***tree***')
+            # if type is "tree" get the subtree
+
 
 if __name__ == '__main__':
-  repos, next = github.getRepos()
-  for repo in repos[0:10]:
-    # repoJson = github.getRepo(repo['url'])
-    sha = github.getRepoSHA(repo['url'])
-    repoJson = github.getRepoContents(repo['url'], sha)
-    processRepoContents(repoJson)
-    # print "content",repoJson
+    repos, is_next = github.get_repos()
+    for repo in repos[0:10]:
+        # repoJson = github.getRepo(repo['url'])
+        sha = github.get_repo_sha(repo['url'])
+        repo_json = github.get_repo_contents(repo['url'], sha)
+        process_repo_contents(repo_json)
+        # print "content",repoJson
 
-  # print next
-  # print repos[1]
-
-
+        # print next
+        # print repos[1]
